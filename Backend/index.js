a66f1f5d8af1df04f0c474be3d59a985ece28282
MuhammadHamza9025 <mhamza9025@gmail.com>
2024-06-24 13:06:28 +0500

    Backend Created

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const port = 2000;
 const app = express()
 app.get('/', async (req, res) => {
-    res.send('Welcome to my server , Waleed yaka')
+    res.send('Welcome to my server , Waleed yaka')//
 })
 
 app.listen(port, () => console.log('Listning to Port', port))
