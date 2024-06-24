39f4cc7155defefd3a5d086edf3e36e1963b869f
MuhammadHamza9025 <mhamza9025@gmail.com>
2024-06-24 11:37:48 +0500

    Created Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
 
 import './App.css';
-
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import About from './Pages/About';
+import Dashboard from './Pages/Dashboard';
+import Signin from './Pages/Signin';
+import Signup from './Pages/Signup';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import Projects from './Pages/Projects';
 function App() {
   return (
     <>
-      <p className='bg-black text-white'> Hamza</p>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/signup' element={<Signup />}></Route>
+          <Route path='/signin' element={<Signin />}></Route>
+          <Route path='/dashboard' element={<Dashboard />}></Route>
+          <Route path='/projects' element={<Projects />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
